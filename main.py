09cc54f4716f15343f8da40d851e388d866b7e19
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-06-01 18:55:35 +0700

    Check user answer whether is correct and display user score

--- main.py
+++ main.py
@@ -3,64 +3,81 @@ import time
 from controller.play_controller import PlayController
 
 class Main:
-  bot_token = '1895994173:AAEOIlOIpYt-8sx5SVu_-cp0smhkVfRO668'
+  bot = telebot.TeleBot('1895994173:AAEOIlOIpYt-8sx5SVu_-cp0smhkVfRO668')
+  question = None
+  user_score = 0
+  is_correct = False
+  
+  # Controllers
+  play_controller = PlayController()
 
   def __init__(self):
     super().__init__()
     
   def main(self):
-    # Create bot
-    bot = telebot.TeleBot(self.bot_token)
-    
-    # Controllers
-    play_controller = PlayController()
- 
     # Message Handlers
     # Start message
-    @bot.message_handler(commands=['start'])
+    @self.bot.message_handler(commands=['start'])
     def start(message):
-      bot.reply_to(
+      self.bot.reply_to(
         message, 
         'Hai, perkenalkan namaku KuisIn Bot. Aku adalah bot untuk bermain kuis pengetahuan umum. Ketik /play untuk bermain'
       )
 
     # Play
-    @bot.message_handler(commands=['play'])
+    @self.bot.message_handler(commands=['play'])
     def play(message):
       chat_id = message.chat.id
+      self.question = None
+      self.user_score = 0
       total_question = 3
 
-      for i in range(0, total_question+1):
-        # Get question and correct answer
-        if i != total_question:
-          question = play_controller.get_question()
-          correct_answer = play_controller.get_correct_answer(question.get_id()).get_text()
+      for i in range(0, total_question):
+        self.is_correct = False
 
-        if i > 0:
-          t = 5
+        # Get question
+        self.question = self.play_controller.get_question()
+        
+        # Display question
+        self.bot.send_message(
+          chat_id, self.question.get_text()
+        )
 
-          # Set timer
-          while t:
-            time.sleep(1)
-            t -= 1
+        # Get user answer and check it
+        self.bot.register_next_step_handler(message, self.check_user_answer)
 
-          if i == total_question:
-            bot.send_message(
-              chat_id, "Yah, waktunya sudah habis"
+        # Set timer
+        t = 20
+        while t:
+          if self.is_correct:
+            break
+
+          time.sleep(1)
+          t -= 1
+
+          if t == 10:
+            self.bot.send_message(
+              chat_id, 'Waktu tersisa 10 detik lagi'
             )
-            play_controller.clear_quest_id_list()
 
-            return
-
-          bot.send_message(
-            chat_id, question.get_text()
+        if i == total_question-1:
+          self.bot.send_message(
+            chat_id, 'Permainan berakhir. Kamu mendapatkan score ' + str(self.user_score)
           )
-        else:
-          bot.send_message(
-            chat_id, question.get_text()
-          )
+          self.play_controller.clear_quest_id_list()
+          
+          return
 
-    bot.polling()
-    
+    self.bot.polling()
+
+  def check_user_answer(self, message):
+    user_answer = message.text
+    correct_answer = self.play_controller.get_correct_answer(self.question.get_id()).get_text()
+
+    if user_answer.lower() == correct_answer.lower():
+      self.user_score += self.question.get_score()
+      self.is_correct = True
+      
+
 main = Main()
 main.main()
